794da0239e1370b027bf3fd44b47c9a41091f1ca
Soongyu Kwon <41620322+s8ngyu@users.noreply.github.com>
2022-09-17 03:59:04 +0900

    init project

---
+++ mugunghwa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mugunghwa
+//
+//  Created by Soongyu Kwon on 9/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mugunghwa/mugunghwaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mugunghwaApp.swift
+//  mugunghwa
+//
+//  Created by Soongyu Kwon on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct mugunghwaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
