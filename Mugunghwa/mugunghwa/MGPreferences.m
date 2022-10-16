fdafdfe2b6a4a892fc4a4e60d75e2cd2663e8150
Soongyu Kwon <me@soongyu.me>
2022-10-16 11:48:58 +0900

    spaces

--- Mugunghwa/mugunghwa/MGPreferences.m
+++ Mugunghwa/mugunghwa/MGPreferences.m
@@ -10,16 +10,16 @@
 @implementation MGPreferences
 - (id)initWithIdentifier:(NSString *)ids {
     self = [super init];
-       if (self) {
-           _identifier = ids;
-           NSString *fullPath = [@"/private/var/mobile/mugunghwa/" stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.plist", ids]];
-           if ([[NSFileManager defaultManager] fileExistsAtPath:fullPath]) {
-               _dictionary = [[NSDictionary dictionaryWithContentsOfFile:fullPath] mutableCopy];
-           } else {
-               _dictionary = [NSMutableDictionary dictionary];
-           }
-       }
-      return self;
+    if (self) {
+        _identifier = ids;
+        NSString *fullPath = [@"/private/var/mobile/mugunghwa/" stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.plist", ids]];
+        if ([[NSFileManager defaultManager] fileExistsAtPath:fullPath]) {
+            _dictionary = [[NSDictionary dictionaryWithContentsOfFile:fullPath] mutableCopy];
+        } else {
+            _dictionary = [NSMutableDictionary dictionary];
+        }
+    }
+    return self;
 }
 
 -(void)updatePlist {
